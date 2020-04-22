d9a44e42b0d07783b33a80b3b0154e5d2acb0b88
Amirhossein Khatabakhsh <46597482+khatabakhsh@users.noreply.github.com>
2020-04-22 22:35:27 +0430

    add html css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="todoapp" id="todoapp">
+        <header className="header">
+          <h1>ToDo</h1>
+          <input
+            type="text"
+            className="new-todo"
+            placeholder="What needs to be done?"
+          />
+        </header>
+        <main className="main">
+          <ul className="todo-list">
+            <li>
+              <div className="view">
+                <input type="checkbox" className="toggle" value="on" />
+                <label>New York</label>
+                <button className="destry" />
+              </div>
+            </li>
+            <li className="completed">
+              <div className="view">
+                <input
+                  type="checkbox"
+                  className="toggle"
+                  value="on"
+                  checked={true}
+                />
+                <label>New York</label>
+                <button className="destry" />
+              </div>
+            </li>
+          </ul>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
