c39a787ebd2ab5d0e22a238ebd3144f8920d463f
Amirhossein_Khatabakhsh <khatabakhsha@gmail.com>
2022-05-16 22:28:24 +0430

    create: src & components folder

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import '../styles/globals.css';
+import '../../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
