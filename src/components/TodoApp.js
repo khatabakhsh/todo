e4e84cac9e15ac2ef10cea09b5309a33f05f0138
Amirhossein_Khatabakhsh <khatabakhsha@gmail.com>
2022-05-19 00:55:20 +0430

    uncontrolled input component

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,11 +1,19 @@
 import { Container, Paper, InputBase, IconButton } from '@mui/material';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import { useRef } from 'react';
 
 export default function TodoApp() {
+  const inputRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputRef.current.value);
+  };
   return (
     <Container maxWidth="md">
       <Paper
         component="form"
+        onSubmit={handleSubmit}
         elevation={2}
         sx={{
           p: '2px 4px',
@@ -17,6 +25,7 @@ export default function TodoApp() {
         <IconButton
           sx={{ p: '10px' }}
           type="submit"
+          value="Submit"
           aria-label="AddCircleOutline"
         >
           <AddCircleOutlineIcon />
@@ -25,6 +34,11 @@ export default function TodoApp() {
           sx={{ ml: 1, flex: 1 }}
           placeholder="Add Task"
           inputProps={{ 'aria-label': 'add task' }}
+          name="add-task"
+          id="addTask"
+          autoFocus
+          type="text"
+          inputRef={inputRef}
         />
       </Paper>
     </Container>
