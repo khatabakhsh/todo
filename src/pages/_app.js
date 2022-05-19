44238a6870df559b9fbd52545da9f652a0cde4f5
Amirhossein_Khatabakhsh <khatabakhsha@gmail.com>
2022-05-19 20:07:11 +0430

    create store in redux

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,13 @@
-/* eslint-disable react/jsx-props-no-spreading */
 import '../../styles/globals.css';
+import { Provider } from 'react-redux';
+import store from '../redux/store';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp;
