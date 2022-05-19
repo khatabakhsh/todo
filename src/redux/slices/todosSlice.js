00419e1f92a8e85f0d4a4f03d0b4f24ca28cb654
Amirhossein_Khatabakhsh <khatabakhsha@gmail.com>
2022-05-19 23:09:46 +0430

    remove comment

--- src/redux/slices/todosSlice.js
+++ src/redux/slices/todosSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 export const todosSlice = createSlice({
   name: 'todos',
-  initialState: [{}],
+  initialState: [],
   reducers: {
     addTodo: (state, action) => {
       state.push({
@@ -16,9 +16,6 @@ export const todosSlice = createSlice({
 
 export const { addTodo } = todosSlice.actions;
 
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state) => state.todos.value)`
 export const selectTodos = (state) => state.todos;
 
 export default todosSlice.reducer;
