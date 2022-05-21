e0b8029e4959613638a4c60f5aabfc1872b975f6
Amirhossein_Khatabakhsh <khatabakhsha@gmail.com>
2022-05-21 19:54:31 +0430

    added: meta viewport tag to disable zoom

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,7 +7,10 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        />
       </Head>
       <Provider store={store}>
         <Component {...pageProps} />
